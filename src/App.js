f8d770325bee48d179e13c7f34f1ff9e954f1fd7
lauragift21 <laurandidi21@gmail.com>
2018-09-22 17:27:48 +0100

    Add eslint and prettier config

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-//  child component
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h1', {}, props.animal),
-    React.createElement('h1', {}, props.breed)
-  ])
-}
+/* global React ReactDOM */
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed)
+  ]);
+};
 
 class App extends React.Component {
-  render () {
-    return React.createElement('div', {}, [
-      React.createElement('h1', {}, 'Adopt Me!'),
+  handleTitleClick() {
+    alert("You clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
       React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Mixed'
+        name: "Luna",
+        animal: "Dog",
+        breed: "Mixed"
       }),
 
       React.createElement(Pet, {
-        name: 'Sudo',
-        animal: 'Dog',
-        breed: 'Caucasian'
+        name: "Sudo",
+        animal: "Dog",
+        breed: "Caucasian"
       }),
 
       React.createElement(Pet, {
-        name: 'Java',
-        animal: 'Dog',
-        breed: 'Bleach'
+        name: "Java",
+        animal: "Dog",
+        breed: "Bleach"
       })
     ]);
-
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
