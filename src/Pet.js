a03c748676068aa36eea77a2ae05feb47bd9a803
lauragift21 <laurandidi21@gmail.com>
2018-09-23 18:06:45 +0100

    Add component lifecycle method

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,24 @@
 import React from "react";
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        </div>
+      </div>
+    );
+  }
+}
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name} </h1>
-      <h1>{props.animal} </h1>
-      <h1>{props.breed} </h1>
-    </div>
-  );
-};
 
 export default Pet;
