057bced1fbdac45109e80cc440ab9807f93c0f34
lauragift21 <laurandidi21@gmail.com>
2018-09-28 06:40:55 +0100

    add react context api :sparkles:

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,8 @@
 import React from "react";
-
 import pf from "petfinder-client";
+import { Consumer } from "./SearchContext";
 import Pet from "./Pet";
+import SearchBox from "./SearchBox";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -17,8 +18,16 @@ class Results extends React.Component {
     };
   }
   componentDidMount() {
+    this.search();
+  }
+  search = () => {
     petfinder.pet
-      .find({ output: "full", location: "Settle, WA" })
+      .find({
+        output: "full",
+        location: this.props.searchParams.location,
+        animal: this.props.searchParams.animal,
+        breed: this.props.searchParams.breed
+      })
       .then(data => {
         let pets;
         if (data.petfinder.pets && data.petfinder.pets.pet) {
@@ -34,10 +43,11 @@ class Results extends React.Component {
           pets
         });
       });
-  }
+  };
   render() {
     return (
       <div className="search">
+        <SearchBox search={this.search} />
         {this.state.pets.map(pet => {
           let breed;
           if (Array.isArray(pet.breeds.breed)) {
@@ -62,4 +72,11 @@ class Results extends React.Component {
   }
 }
 
-export default Results;
+// add consumer to export
+export default function ResultsWithContext(props) {
+  return (
+    <Consumer>
+      {context => <Results {...props} searchParams={context} />}
+    </Consumer>
+  );
+}
