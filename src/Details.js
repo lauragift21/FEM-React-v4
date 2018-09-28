9d78335b7c2d6547b87e586cc689cf7353364621
lauragift21 <laurandidi21@gmail.com>
2018-09-28 06:53:09 +0100

    Add portals code :fire:

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -11,7 +12,8 @@ const petfinder = pf({
 class Details extends React.Component {
   //  initial state of the app
   state = {
-    loading: true
+    loading: true,
+    showModal: false
   };
   componentDidMount() {
     petfinder.pet
@@ -42,6 +44,8 @@ class Details extends React.Component {
         navigate("/");
       });
   }
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
   render() {
     if (this.state.loading) {
       //display custom loader
@@ -62,17 +66,35 @@ class Details extends React.Component {
         </div>
       );
     }
-    const { animal, breed, location, description, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      showModal
+    } = this.state;
 
     return (
       <div className="details">
-      <Carousel  media={media} />
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>
             {animal} - {breed} - {location}{" "}
           </h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </Modal>
+          ) : null
+          }
         </div>
       </div>
     );
